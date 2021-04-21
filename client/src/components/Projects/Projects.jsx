1a0ac8ed35d2ca3cec9ecb0ea9d8a61652688bed
Michael Pulido <michaelpulido@Michaels-MacBook-Pro.local>
2021-04-20 22:37:53 -0400

    finished with media queries

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -1,20 +1,18 @@
 import SportsBook from "../../sportsbooks.png";
-import GolfCrunch from "../../golfcrunch.png";
-import GolfCrunch2 from "../../golfcrunch2.png";
 import SongsYou from "../../songsyou.png";
 import SalesSkywalker from "../../salesskywalker.png";
-import GolfCrunch3 from "../../golfcrunch3.png";
 import "./Projects.css";
 import { useState } from "react";
 import PictureArray from "../PicturesArray/PictureArray";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
+import GolfCrunch from "../../golfcrunchcombined.png"
 
 const Projects = (props) => {
   const projects = [
     {
       website: "https://songsu.netlify.app/",
       github: "https://github.com/jstiles44/P3-Playlist-App/tree/devpmvp",
-      pictures: [SongsYou],
+      picture: SongsYou,
       title: "SongsYOU",
       info: "React | Express | JavaScript | Mongoose | MongoDB | Heroku | CSS",
       caption:
@@ -23,29 +21,29 @@ const Projects = (props) => {
     {
       website: "https://sales-skywalker.netlify.app/",
       github: "https://github.com/mtpulido/Sales-Skywalker",
-      pictures: [SalesSkywalker],
+      picture: SalesSkywalker,
       title: "Sales Skywalker",
       info: "Ruby on Rails | JavaScript | React | PostgreSQL | Heroku | CSS",
       caption:
         "A full-stack application functioning as a play-off of Sales Force. It shows users their relevant analytics to improve their performance as well as stay organized in their daily tasks.",
     },
     {
-      website: "https://golf-crunch.netlify.app/",
-      github: "https://github.com/mtpulido/Pulido-SportsBook",
-      pictures: [GolfCrunch2, GolfCrunch3, GolfCrunch],
-      title: "Golf Crunch",
-      info: "React | JavaScript | JSX | CRUD |Airtable | CSS",
-      caption:
-        "A mobile application that helps golfers of all skill levels keep track of their statistics, access customized analysis, and view the rounds their friends have submitted.",
-    },
-    {
       website: "https://mtpulido.github.io/Pulido-SportsBook/",
       github: "https://github.com/mtpulido/The-Golf-Crunch-App",
-      pictures: [SportsBook],
+      picture: SportsBook,
       title: "The SportsBook",
       info: "JavaScript | The Odds API | HTML | CSS",
       caption:
         "I built the SportsBook 2 weeks into learning how to code. It's a web application that allows users to see the different gambling odds for the NBA pulled from a 3rd party API",
+    },
+    {
+      website: "https://golf-crunch.netlify.app/",
+      github: "https://github.com/mtpulido/Pulido-SportsBook",
+      picture: GolfCrunch,
+      title: "Golf Crunch",
+      info: "React | JavaScript | JSX | CRUD |Airtable | CSS",
+      caption:
+        "A mobile application that helps golfers of all skill levels keep track of their statistics, access customized analysis, and view the rounds their friends have submitted.",
     },
   ];
 
@@ -61,10 +59,6 @@ const Projects = (props) => {
     projectIndex !== 0 ? setProjectIndex((prevState) => prevState - 1) : setProjectIndex(projects.length - 1);
   };
 
-  const PicturesJSX = projects[projectIndex].pictures.map((picture, index) => (
-    <PictureArray picture={picture} index={index}/>
-  ));
-
   return (
     <div className="projects-container" id="projects">
           <SwitchTransition>
@@ -73,7 +67,16 @@ const Projects = (props) => {
           addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
           classNames={fades}
       >
-      <div className="carousel-container">
+          <div className="carousel-container">
+            <div className="multi-images-project"><PictureArray picture={projects[projectIndex].picture}/></div>
+            <div className="project-buttons">
+        <div onClick={decrementIndex} className="back-button">
+          back
+        </div>
+              <div onClick={incrementIndex} className="next-button">
+            next
+        </div>
+        </div>
         <div className="project-title">{projects[projectIndex].title}</div>
         <div className="project-info">{projects[projectIndex].info}</div>
         <div className="project-links">
@@ -93,16 +96,7 @@ const Projects = (props) => {
             </a>
           </div>
         <div className="project-caption">{projects[projectIndex].caption}</div>
-        <div className="multi-images-project">{PicturesJSX}</div>
-
-        <div className="project-buttons">
-        <div onClick={decrementIndex} className="back-button">
-          back
-        </div>
-        <div onClick={incrementIndex} className="next-button">
-            next
-        </div>
-        </div>
+       
           </div>
           </CSSTransition>
       </SwitchTransition>
